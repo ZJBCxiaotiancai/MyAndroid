4f1eb5a5338ca3594c4579a2f03993c84b1d0c86
xiaotiancai <925346521@qq.com>
2023-03-02 20:15:27 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/zjbc/wlw56122100738/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.zjbc.wlw56122100738;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
